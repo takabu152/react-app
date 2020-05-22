e8da23a45929bb17f034923c2641a26665c28afb
takabu152 <yamashitat@libre-code.com>
2020-05-22 15:54:51 +0900

    Add Booklist

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Booklist from './components/Booklist'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>react app</h1>
+      <Booklist/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Booklist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Booklist = props => {
+    return (
+        <div>
+            <p>this is book list component</p>
+        </div>
+    )
+}
+
+export default Booklist
