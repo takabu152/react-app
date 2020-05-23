a7d4f165051ffc69f4d36c9f4ff00931c3a882d4
takabu152 <yamashitat@libre-code.com>
2020-05-24 08:39:14 +0900

    react-router-set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Booklist from './components/Booklist'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 
 const App = () => {
+  const languages = ['React','Vue','Angular']
   return (
-    <div>
-      <h1>react app</h1>
-      <Booklist/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>react app</h1>
+
+        <Route exact path = '/' component={Booklist} />
+        <Route exact path = '/vue' component={Booklist} />
+        <Route exact path = '/angular' component={Booklist} />
+        {/* <Booklist language = {languages[0]} />
+        <Booklist language = {languages[1]} />
+        <Booklist language = {languages[2]} /> */}
+      </div>
+    </BrowserRouter>
+    
   )
 }
 
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Booklist = props => {
     return (
         <div>
-            <p>this is book list component</p>
+            <p>this is {props.language} book list component</p>
         </div>
     )
 }
