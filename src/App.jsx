6fe8fd7def619c89f6386d12f12c0a6e4cdab75f
takabu152 <yamashitat@libre-code.com>
2020-05-24 10:54:10 +0900

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import Booklist from './components/Booklist'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+
+const getDataFromAPI = async keyword => {
+  const requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=initle:'
+  const result = await axios.get(`${requestUrl}${keyword}`)
+  console.log(result)
+  return result
+}
 
 
 const App = () => {
-  const languages = ['React','Vue','Angular']
+  const languages = ['React', 'Vue', 'Angular']
   return (
     <BrowserRouter>
       <div>
         <h1>react app</h1>
-
-        <Route exact path = '/' component={Booklist} />
-        <Route exact path = '/vue' component={Booklist} />
-        <Route exact path = '/angular' component={Booklist} />
+        <ul>
+          <li><Link to='/'>React</Link></li>
+          <li><Link to='/vue'>Vue</Link></li>
+          <li><Link to='/angular'>Angular</Link></li>
+        </ul>
+        <Route
+          exact
+          path='/'
+          render={
+            props =>
+              <Booklist language={languages[0]} getData={keyword => getDataFromAPI(keyword)} />
+          }
+        />
+        <Route
+          exact
+          path='/vue'
+          render={
+            props =>
+              <Booklist language={languages[1]} getData={keyword => getDataFromAPI(keyword)} />
+          }
+        />
+        <Route
+          exact
+          path='/angular'
+          render={
+            props =>
+              <Booklist language={languages[2]} getData={keyword => getDataFromAPI(keyword)} />
+          }
+        />
         {/* <Booklist language = {languages[0]} />
         <Booklist language = {languages[1]} />
         <Booklist language = {languages[2]} /> */}
       </div>
     </BrowserRouter>
-    
+
   )
 }
 
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Booklist = props => {
+
+    const [bookData, setBookData] = useState(null)
+
+    useEffect(() => { const result = props.getData?.(props.language).then(response => setBookData(response)) }, [props])
+    console.log(bookData)
+
     return (
         <div>
-            <p>this is {props.language} book list component</p>
+            <ul>
+                {
+                    bookData === null
+                    ? <p>now loading...</p>
+                    : bookData.data.items.map(x => <li>{x.volumeInfo.title}</li>) 
+                }
+            </ul>
         </div>
     )
 }
